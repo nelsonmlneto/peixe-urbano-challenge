aaf733f263d1db0b5ee81e25b418f2bd5e605da0
nelson.nextt <nelson.mariano@nextt.com.br>
2019-02-10 16:03:18 -0200

    Creating home page.

--- src/main/java/br/com/peixeurbano/challenge/controllers/HomeController.java
+++ src/main/java/br/com/peixeurbano/challenge/controllers/HomeController.java
@@ -1,7 +1,14 @@
 package br.com.peixeurbano.challenge.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
 }
